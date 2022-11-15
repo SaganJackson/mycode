ca1506eb93f2a2ab425d10b4f28fc88c885fddc5
sagan <sagan.jacksonmoses@gmail.com>
2022-11-15 21:49:55 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#create a list containing 3 items
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+#display the first item
+print("The first item in the list (IP): " + my_list[0] )
+
+#display the second item
+print("The second item in the list (port): " + str(my_list[1]) )
+
+#display the third itemm
+print("The last item in the list (state): " + my_list[2] )
+
